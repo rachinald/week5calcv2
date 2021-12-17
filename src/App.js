c5e973d05f8fa2c610bb423eec67da693cbb37ee
Rachael Jane <rachinald@gmail.com>
2021-12-17 18:49:10 +0000

    i

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const theButtons = [
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "9",
+    "-",
+    ".",
+    "0",
+    "C",
+    "=",
+  ];
+
+  const [screen, setScreen] = useState("");
+
+  // const config = {};
+  // const math = create(all, config);
+
+  const theButtonsDoThings = (button) => {
+    if (button === "=") {
+      setScreen(evaluate(screen));
+    } else if (button === "C") {
+      setScreen("");
+    } else {
+      setScreen(screen + button);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="calc">
+          <div className="screen">{screen}</div>
+          <div className="buttons">
+            {theButtons.map((button, index) => {
+              return (
+                <h2
+                  className="singleButton"
+                  // In order to pass a value as a parameter through the onClick handler we pass in an arrow function which returns a call to the theButtonsDoThings function.
+                  onClick={() => {
+                    theButtonsDoThings(button);
+                  }}
+                  key={index}
+                >
+                  {button}
+                </h2>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
